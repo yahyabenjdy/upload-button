e453a8341b2942aa7dd22db77789e49099f765e8
Yahya Benjdy <yahyabenjdy044@gmail.com>
2023-11-28 19:28:22 +0100

    Update main.js

--- upload button/main.js
+++ upload button/main.js
@@ -1,29 +1,8 @@
-body {
-  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-color: #2b2d42;
-  color: #edf2f4;
-}
-h1 {
-  font-size: 3rem;
-  font-weight: 800;
-}
-img {
-  margin: 2rem;
-  border-radius: 15px;
-  width: 30%;
-}
-label {
-  background-color: #fff;
-  color: #222;
-  padding: 1rem 2rem;
-  border-radius: 15px;
-}
-p {
-  margin: 2rem;
-  text-align: center;
-}
+const realBtn = document.getElementById("real-btn");
+const tempImg = document.getElementById("temp-img");
+const tempLabel = document.getElementById("temp-label");
+
+realBtn.addEventListener("change", () => {
+  tempLabel.textContent = realBtn.files[0].name;
+  tempImg.src = URL.createObjectURL(realBtn.files[0]);
+});
